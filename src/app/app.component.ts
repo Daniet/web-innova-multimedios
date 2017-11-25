1d64c620f7a6d730b8804f4298c6eed994fbbfe1
Dan Dy <danie7h@gmail.com>
2017-11-25 16:36:53 -0500

    Hasta las 4:30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,47 @@ declare var d3:any
 
 export class AppComponent {
   title = 'app';
+  menuItem:any = [{
+    link: '',
+    name: 'Incio'
+  },{
+    link: '',
+    name: 'Productos'
+  },{
+    link: '',
+    name: 'Trabajos'
+  },{
+    link: '',
+    name: 'Contactenos'
+  }]
+
+  angleToPi(angle:number){
+    return (angle * Math.PI)/ 180
+  }
+
+  setPosY(data){
+    return Math.cos(data.PI) * data.h
+  }
+
+  setPosX(data){
+    return Math.sin(data.PI) * data.h
+  }
+
+  menu(){
+    let angle = 180 / (this.menuItem.length + 1)
+  }
 
   menuShow(){
-    $('.menu').css({
-      width: '100%'
-    })
+    $('.menu').addClass('menu-show')
   }
+
+  menuhidden(){
+    $('.menu').removeClass('menu-show')
+  }
+
+  ngOnInit(){
+    this.menuShow()
+    this.menu()
+  }
+
 }
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -9,9 +9,91 @@ declare var $:any
 })
 export class JobsComponent implements OnInit {
 
+  products:any = [{
+    name: 'Producto 1',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 2',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 3',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 4',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 5',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 6',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 7',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 8',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 9',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 10',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 11',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 12',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 13',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 14',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 15',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 16',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 17',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 18',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 19',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 20',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  },{
+    name: 'Producto 21',
+    description: 'pidha hasdio hasodihouasdh asdb iasdui asb asbi asdb asb asb oasd oasd o'
+  }]
+
+  hoverProduct:any = {
+    title: '',
+    text: ''
+  }
+
+  preview(item){
+    this.hoverProduct = item
+  }
+
   constructor() { }
 
   ngOnInit() {
+
+    setTimeout(()=>{
+      $('#description-products').removeClass('description-products-hidden')
+      $('#description-products').addClass('description-products-show')
+
+      $('#list-products').removeClass('list-products-hidden')
+      $('#list-products').addClass('list-products-show')
+    }, 1000)
   }
 
 }
